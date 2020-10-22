1e8b2c456fb503791d5a867ebf8973d0df4df71f
Juan <jmartinez6784@yahoo.com>
2020-10-21 21:14:46 -0700

    added styling

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import StateList from "./StateList";
 
 const Weather = () => {
+  const [loading, setLoading] = useState(true);
   const [weatherInfo, setWeatherInfo] = useState({
     city: "...",
+    state: "...",
     temperature: "...",
     feelsLike: "...",
     humidity: "...",
-    tempMax: "...",
-    tempMin: "...",
     wind: "...",
   });
 
@@ -20,22 +20,23 @@ const Weather = () => {
 
   const getWeather = async (city, state) => {
     try {
-      console.log("INFO", city, state);
       const response = await fetch(
         `//api.openweathermap.org/data/2.5/weather?q=${city},${state}&units=imperial&appid=ca3ff5d7732b4af049db6f42dd06e767`,
         { mode: "cors" }
       );
 
       const data = await response.json();
-      console.log(data);
+
+      if (data) {
+        setLoading(false);
+      }
 
       setWeatherInfo({
         city: data.name,
+        state: "",
         temperature: data.main.temp,
         feelsLike: data.main.feels_like,
         humidity: data.main.humidity,
-        tempMax: data.main.temp_max,
-        tempMin: data.main.temp_min,
         wind: data.wind.speed,
       });
     } catch (error) {
@@ -48,13 +49,11 @@ const Weather = () => {
   };
 
   const handleSearchClick = (city, state) => {
-    console.log("BEfORE", city, state);
     setLocation({
       city: city,
       state: state,
     });
     getWeather(city, state);
-    console.log("After", city, state);
     setCtrlSearch("");
   };
 
@@ -63,61 +62,89 @@ const Weather = () => {
     getWeather(location.city.split(" ").join("+"), location.state);
   }, [location]);
 
-  return (
-    <div>
-      <h1>Odin Weather</h1>
-      <h2>City: {weatherInfo.city}</h2>
-      <h2>Current Temperature: {weatherInfo.temperature}°F</h2>
-      <h2>Feels like: {weatherInfo.feelsLike}°F</h2>
-      <h2>Humidity: {weatherInfo.humidity}%</h2>
-      <h2>Wind speed: {weatherInfo.wind}mph</h2>
-      <h2>
-        Min: {weatherInfo.tempMin}°F / Max: {weatherInfo.tempMax}°F
-      </h2>
-      <br />
-      <div>
-        <label>Change City:</label>
-        <br />
-        <input
-          id="search-input"
-          type="text"
-          onChange={handleSearchInput}
-          value={ctrlSearch}
-          placeholder={location.city}
-        />
-
-        <label>State:</label>
-
-        <select id="state-dropdown" name="state-selection">
-          {StateList.map((state) => {
-            return (
-              <option
-                selected={state.name === "california" ? true : false}
-                key={state.id}
-                value={state.name}
-              >
-                {state.name.replace(/\w\S*/g, (w) =>
-                  w.replace(/^\w/, (c) => c.toUpperCase())
-                )}
-              </option>
-            );
-          })}
-        </select>
-
-        <button
-          onClick={() =>
-            handleSearchClick(
-              document.getElementById("search-input").value,
-              document.getElementById("state-dropdown").value
-            )
-          }
-        >
-          Search
-        </button>
+  if (loading) {
+    return (
+      <div id="weather-container">
+        <h1 id="loading">Loading...</h1>
       </div>
-      <h2>Current search: {ctrlSearch}</h2>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div id="weather-container">
+        <h1 className="title">Odin Weather</h1>
+
+        <div id="weather-info-container">
+          <h1 id="current-temp" className="weather-info">
+            {Math.round(parseInt(weatherInfo.temperature))}°F
+          </h1>
+          <h2 id="city-name" className="weather-info">
+            {weatherInfo.city}
+          </h2>
+          <h2 className="weather-info">
+            Feels like {Math.round(parseInt(weatherInfo.feelsLike))}°F
+          </h2>
+          <h2 className="weather-info">
+            Humidity {Math.round(parseInt(weatherInfo.humidity))}%
+          </h2>
+          <h2 className="weather-info">
+            Wind speed {Math.round(parseInt(weatherInfo.wind))}mph
+          </h2>
+        </div>
+
+        <div id="inputs-container">
+          <div className="input-label">
+            <label className="search-label">Change City:</label>
+            <input
+              id="search-input"
+              className="search-inputs"
+              type="text"
+              onChange={handleSearchInput}
+              value={ctrlSearch}
+              placeholder={location.city.replace(/\w\S*/g, (w) =>
+                w.replace(/^\w/, (c) => c.toUpperCase())
+              )}
+            />
+          </div>
+
+          <div className="input-label">
+            <label className="search-label">State:</label>
+
+            <select
+              id="state-dropdown"
+              className="search-inputs"
+              name="state-selection"
+            >
+              {StateList.map((state) => {
+                return (
+                  <option
+                    selected={state.name === "california" ? true : false}
+                    key={state.id}
+                    value={state.name}
+                  >
+                    {state.name.replace(/\w\S*/g, (w) =>
+                      w.replace(/^\w/, (c) => c.toUpperCase())
+                    )}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+
+          <button
+            id="search-btn"
+            onClick={() =>
+              handleSearchClick(
+                document.getElementById("search-input").value.toLowerCase(),
+                document.getElementById("state-dropdown").value
+              )
+            }
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Weather;
