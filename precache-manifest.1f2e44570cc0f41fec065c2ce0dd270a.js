6fef70dd7c85020665245ff580b69658546d87ee
jmart6784 <jmartinez6784@yahoo.com>
2021-01-21 12:37:28 -0800

    Updates

---
+++ precache-manifest.1f2e44570cc0f41fec065c2ce0dd270a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9de6e6afb5337df4da4ac4557ca8ad2",
+    "url": "/odin-weather/index.html"
+  },
+  {
+    "revision": "271299dfa64491875d23",
+    "url": "/odin-weather/static/js/2.84ceb121.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/odin-weather/static/js/2.84ceb121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e82ab135abee8c730e44",
+    "url": "/odin-weather/static/js/main.611a1d40.chunk.js"
+  },
+  {
+    "revision": "72b6d9ba8e616d5b52c6",
+    "url": "/odin-weather/static/js/runtime-main.c91a5978.js"
+  }
+]);
